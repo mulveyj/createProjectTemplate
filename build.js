eed034c015c80303f7d6889147a4cdb5d1b6d5c8
mulveyj <github@joemulvey.com>
2017-06-07 17:17:05 +0100

    basic directory and file build

--- build.js
+++ build.js
@@ -1,37 +1,103 @@
 const fs = require('fs');
-const defaultLocation = '/Users/joemulvey/Northcoders/wk5/Projects';
-const configs = '/Users/joemulvey/Northcoders/wk5/projectBuilder/defaults'
-const EventEmitter = require('events');
 
-function standardCB (err) {
-    if (err) {
-        console.log('Shiiiiiiit!!!', err);
+
+function createProject(name) {
+    const defaultLocation = '/Users/joemulvey/Northcoders/wk5/Projects';
+    const configs = '/Users/joemulvey/Northcoders/wk5/projectBuilder/defaults';
+    // Task 1 - take input [Project Name] from command line argv and assign to const
+    // const name = process.argv[2];
+    const projDir = defaultLocation + '/' + name;
+    const specDir = projDir + '/spec';
+    const specFile = specDir + '/' + name + '.spec.js';
+    const srcDir = projDir + '/src';
+    const srcFile = srcDir + '/' + name + '.js';
+
+    function checkExists(name) {
+        return new Promise(function (resolve) {
+            fs.access(defaultLocation + '/' + name, fs.constants.R_OK, function (err) {
+                if (!err) throw new Error('Project already exists');
+                resolve();
+            });
+        });
     }
+
+    function makeNewDir (dir) {
+        return new Promise (function (resolve) {
+            fs.mkdir(dir, function (err) {
+                if (!err) resolve();
+                if (err) throw new Error (err);
+            });
+        });
+    }
+
+    function makeFile (file, writeData = '') {
+        return new Promise (function (resolve) {
+            fs.writeFile(file, writeData,function (err) {
+                if(!err) resolve();
+                if (err) throw new Error(err);
+            });
+        });
+    }
+
+Promise.resolve(name)
+// Task 2 - async check if [Project Name] exists in default location
+    .then(function (name) {
+        checkExists(name);
+    })
+// Task 4 - asyc create directory [Project Name] and await callback
+    .then(function () {
+        makeNewDir(projDir);
+    })
+// Task 6a - async make directory spec and await callback
+    .then(function () {
+        makeNewDir (specDir);
+    })
+// Task 6b - async create directory src and await callback
+    .then(function () {
+        makeNewDir (srcDir);
+    })
+// Task 11 - on 6a, async create spec/[Project Name].spec.js and await callback
+// Task 12 - on 11, async append standard expect line into [Project Name].spec.
+    .then(function () {
+        makeFile(specFile, 'const {expect} = require(\'chai\');');
+    })
+// Task 13 - on 6b, async create src/[Project Name].js
+    .then(function () {
+        makeFile(srcFile, '');
+    })
+    .then(function () {
+        console.log('Amazingly, we\'re there');
+    }).catch(function (whatevs) {
+        console.log(whatevs);
+    });
 }
 
-// Task 1 - take input [Project Name] from command line argv and assign to const
-const projName = process.argv[2];
-const termCode = false
-
-// Task 2 - async check if [Project Name] exists in default location
+createProject(process.argv[2]);
 /*
-fs.access(defaultLocation + '/' + projName, fs.constants.R_OK, function (err, res) {
-    if (!err) console.log('Project already exists');
-    termCode = true;
-});
-*/
+
+
+
+
+
+
+
+
+
 
 // Task 3 - on Task 2 completion, if not already existing, go to default location
 
-// Task 4 - asyc create directory [Project Name] and await callback
-const projDir = defaultLocation + '/' + projName;
-fs.mkdir(projDir,standardCB);
+
+
 
 // Task 5 - on Task 4 move into directory [Project Name]
 
-// Task 6a - async make directory spec and await callback
 
+const specDir = projDir + '/spec';
+//fs.mkdir(specDir,standardCB);
+ 
 // Task 6b - async create directory src and await callback
+const srcDir = projDir + '/src';
+//fs.mkdir(srcDir,standardCB);
 
 // Task 7 - async copy configs/.editorconfig into . and await callback
 
@@ -42,10 +108,18 @@ fs.mkdir(projDir,standardCB);
 // Task 10 - async copy configs/.package.json into . amd await callback
 
 // Task 11 - on 6a, async create spec/[Project Name].spec.js and await callback
-
 // Task 12 - on 11, async append standard expect line into [Project Name].spec.js
 
+const specFile = specDir + '/' + name + '.spec.js';
+
+fs.writeFile(specFile, 'const {expect} = require(\'chai\');',standardCB);
+
+
 // Task 13 - on 6b, async create src/[Project Name].js
+const srcFile = srcDir + '/' + name + '.js';
+
+fs.open(srcFile,'w',standardCB);
+
 
 // Task 14 - async (?) execute npm install on . and await callback
 
@@ -54,3 +128,4 @@ fs.mkdir(projDir,standardCB);
 // Task 16 - console execute shell command tree
 
 // Task 17 code . 
+*/

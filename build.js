d1f6dfcc5f3002b7bfe3855fc671ab22067cb5a8
mulveyj <github@joemulvey.com>
2017-06-07 13:00:36 +0100

    Initial setup and pseudocode in build

---
+++ build.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const defaultLocation = '~/Northcoders/wk5/Projects';
+const configs = '~/Northcoders/wk5/projectBuilder/defaults'
+
+// Task 1 - take input [Project Name] from command line argv and assign to const
+
+// Task 2 - Check if [Project Name] exists in default location
+
+// Task 3 - go to default location
+
+// Task 4 - asyc create directory [Project Name] and await callback
+
+// Task 5 - on Task 4 move into directory [Project Name]
+
+// Task 6a - async make directory spec and await callback
+
+// Task 6b - async create directory src and await callback
+
+// Task 7 - async copy configs/.editorconfig into . and await callback
+
+// Task 8 - async copy configs/.eslintrc into . and await callback
+
+// Task 9 - async copy configs/.gitignore into . and await callback
+
+// Task 10 - async copy configs/.package.json into . amd await callback
+
+// Task 11 - on 6a, async create spec/[Project Name].spec.js and await callback
+
+// Task 12 - on 11, async append standard expect line into [Project Name].spec.js
+
+// Task 13 - on 6b, async create src/[Project Name].js
+
+// Task 14 - async (?) execute npm install on . and await callback
+
+// Task 15 - async (?) git init .
+
+// Task 16 - console execute shell command tree
+
+// Task 17 code . 
